9a53a77ec80e6f77fb76d14f7ebbdd2ac351af01
soofoove <pe4enkus@gmail.com>
2025-04-06 23:17:48 +0300

    initial commit

---
+++ Test.Api/ClaimsConstants.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Test.Api;
+
+public static class ClaimsConstants
+{
+    public const string Scope = "http://schemas.microsoft.com/identity/claims/scope";
+}
---
+++ Test.Api/Program.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Test.Api;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options => 
+    {
+        options.Authority = "https://login.microsoftonline.com/4e726671-6def-427b-a724-60f7c409aaa2";
+        options.Audience = "api://6ad5eab3-37b8-47bf-afd7-ff468bb6ac65";
+    });
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthorizationBuilder()
+  .AddPolicy("weather", policy =>
+        policy
+            .RequireClaim(ClaimsConstants.Scope, "Weather.Read"));
+    
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", (HttpContext context) =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.RequireAuthorization();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
